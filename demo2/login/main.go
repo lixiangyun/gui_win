30317c08f19070bd4c8ff9f200117c05571d3461
lixiangyun <linimbus@126.com>
2020-09-27 20:01:34 +0800

    update

--- demo2/login/main.go
+++ demo2/login/main.go
@@ -6,7 +6,6 @@ import (
 	. "github.com/lxn/walk/declarative"
 	"log"
 	mathrand "math/rand"
-	"sync"
 	"time"
 )
 
@@ -28,21 +27,21 @@ func randFloat() float32 {
 }
 
 func init()  {
-	var once sync.Once
-
 	go func() {
 		for {
-			time.Sleep(time.Second)
-
-			once.Do(func() {
+			if mainWindow != nil && mainWindow.Visible() {
 				mainWindow.SetSize(walk.Size{500, 301})
-				mainWindow.SetSize(walk.Size{500, 300})
-			})
+				break
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
 
+		for {
+			time.Sleep(time.Second)
 			statusBar.SetText("normal")
 			networkBar.SetText("default")
 			upFlowBar.SetText(fmt.Sprintf("Up:%.1fkb/s", randFloat()))
-			downFlowBar.SetText(fmt.Sprintf("Up:%.1fkb/s", randFloat()))
+			downFlowBar.SetText(fmt.Sprintf("Down:%.1fkb/s", randFloat()))
 		}
 	}()
 }
@@ -72,16 +71,169 @@ func StatusBarInit() []StatusBarItem {
 	}
 }
 
+type LoginInfo struct {
+	User     string
+	Password string
+	Remenber bool
+	Auto     bool
+}
+
+func LoginDialog(loginOld *LoginInfo)  *LoginInfo {
+	var dlg *walk.Dialog
+	var db *walk.DataBinder
+	var acceptPB, cancelPB *walk.PushButton
+	var remenber, auto *walk.RadioButton
+	var result *walk.Label
+
+	_, err := Dialog{
+		AssignTo:      &dlg,
+		Title:         "Login",
+		Icon:          walk.IconShield(),
+		DefaultButton: &acceptPB,
+		CancelButton:  &cancelPB,
+		DataBinder: DataBinder{
+			AssignTo:       &db,
+			Name:           "login",
+			DataSource:     loginOld,
+			ErrorPresenter: ToolTipErrorPresenter{},
+		},
+		MinSize: Size{300, 220},
+		Layout:  VBox{},
+		Children: []Widget{
+			Composite{
+				Layout: Grid{Columns: 2},
+				Children: []Widget{
+					Label{
+						Text: "User:",
+					},
+					LineEdit{
+						Text: Bind("User"),
+					},
+					Label{
+						Text: "Password:",
+					},
+					LineEdit{
+						Text: Bind("Password"),
+						PasswordMode: true,
+					},
+					Composite{
+						Layout: HBox{},
+						Children: []Widget{
+							RadioButton{
+								AssignTo: &remenber,
+								Text: "记住密码",
+								OnClicked: func() {
+									if loginOld.Remenber == true {
+										loginOld.Remenber = false
+										remenber.SetChecked(false)
+									} else {
+										loginOld.Remenber = true
+										remenber.SetChecked(true)
+									}
+									log.Printf("value : %v", remenber.Value())
+								},
+							},
+						},
+					},
+					Composite{
+						Layout: HBox{},
+						Children: []Widget{
+							RadioButton{
+								AssignTo: &auto,
+								Text: "自动登陆",
+								OnClicked: func() {
+									if loginOld.Auto == true {
+										loginOld.Auto = false
+										auto.SetChecked(false)
+									} else {
+										loginOld.Remenber = true
+										remenber.SetChecked(true)
+
+										loginOld.Auto = true
+										auto.SetChecked(true)
+									}
+								},
+							},
+						},
+					},
+					Label{
+						AssignTo: &result,
+						Text: "",
+						MinSize: Size{Height: 15},
+					},
+				},
+			},
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					PushButton{
+						AssignTo: &acceptPB,
+						Text:     "OK",
+						OnClicked: func() {
+							if err := db.Submit(); err != nil {
+								result.SetText(err.Error())
+								return
+							}
+							if loginOld.User == "" {
+								result.SetText("请输入用户名")
+								return
+							}
+							if loginOld.Password == "" {
+								result.SetText("请输入密码")
+								return
+							}
+							acceptPB.SetEnabled(false)
+							cancelPB.SetEnabled(false)
+							result.SetText("登陆中..")
+
+							go func() {
+								time.Sleep(time.Second)
+								result.SetText("登陆失败")
+
+								//dlg.Accept()
+								acceptPB.SetEnabled(true)
+								cancelPB.SetEnabled(true)
+							}()
+						},
+					},
+					PushButton{
+						AssignTo:  &cancelPB,
+						Text:      "Cancel",
+						OnClicked: func() { dlg.Cancel() },
+					},
+				},
+			},
+		},
+	}.Run(mainWindow)
+
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+
+	log.Printf("%v\n", loginOld)
+
+	return loginOld
+}
+
+var loginInfo LoginInfo
+
 func MenuBarInit() []MenuItem {
 	return []MenuItem{
 		Menu{
-			Text: "&Setting",
+			Text: "&File",
 			Items: []MenuItem{
 				Action{
-					Text:        "&login",
+					Text:     "&Login",
+					OnTriggered: func() {
+						LoginDialog(&loginInfo)
+					},
 				},
-				Menu{
-					Text:     "Recent",
+				Action{
+					Text:     "&Setting",
+				},
+				Action{
+					Text:     "&Export",
 				},
 				Separator{},
 				Action{
@@ -93,6 +245,10 @@ func MenuBarInit() []MenuItem {
 		Menu{
 			Text: "&Help",
 			Items: []MenuItem{
+				Action{
+					Text:        "&Register",
+				},
+				Separator{},
 				Action{
 					Text:        "About",
 					OnTriggered: showAboutBoxAction,
