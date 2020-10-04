e19fd5d1d4ef3f66169a8fa45529af8cbbf589a0
lixiangyun <linimbus@126.com>
2020-10-04 19:20:41 +0800

    Update notifyicon.go

--- demo/notifyicon/notifyicon.go
+++ demo/notifyicon/notifyicon.go
@@ -5,33 +5,27 @@
 package main
 
 import (
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
 	"log"
 )
 
-import (
-	"github.com/lxn/walk"
-)
+var mainWindow *walk.MainWindow
 
-func main() {
-	// We need either a walk.MainWindow or a walk.Dialog for their message loop.
-	// We will not make it visible in this example, though.
-	mw, err := walk.NewMainWindow()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func notify()  {
 	// We load our icon from a file.
 	icon, err := walk.Resources.Icon("../img/stop.ico")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+
+
 	// Create the notify icon and make sure we clean it up on exit.
-	ni, err := walk.NewNotifyIcon(mw)
+	ni, err := walk.NewNotifyIcon(mainWindow)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer ni.Dispose()
 
 	// Set the icon and a tool tip text.
 	if err := ni.SetIcon(icon); err != nil {
@@ -58,10 +52,16 @@ func main() {
 
 	// We put an exit action into the context menu.
 	exitAction := walk.NewAction()
-	if err := exitAction.SetText("E&xit"); err != nil {
+	if err := exitAction.SetText("&MainWindow"); err != nil {
 		log.Fatal(err)
 	}
-	exitAction.Triggered().Attach(func() { walk.App().Exit(0) })
+
+	exitAction.Triggered().Attach(func() {
+		mainWindow.SetVisible(true)
+
+		//walk.App().Exit(0)
+	})
+
 	if err := ni.ContextMenu().Actions().Add(exitAction); err != nil {
 		log.Fatal(err)
 	}
@@ -74,6 +74,65 @@ func main() {
 	// Now that the icon is visible, we can bring up an info balloon.
 	if err := ni.ShowInfo("Walk NotifyIcon Example", "Click the icon to show again."); err != nil {
 		log.Fatal(err)
+	}
+
+	mainWindow.SetVisible(false)
+}
+
+func main() {
+	// We need either a walk.MainWindow or a walk.Dialog for their message loop.
+	// We will not make it visible in this example, though.
+
+	icon1, err := walk.NewIconFromFile("../img/check.ico")
+	if err != nil {
+		log.Fatal(err)
+	}
+	icon2, err := walk.NewIconFromFile("../img/stop.ico")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var sbi *walk.StatusBarItem
+
+	mw := &MainWindow{
+		AssignTo: &mainWindow,
+		Title:   "Walk Statusbar Example",
+		MinSize: Size{600, 200},
+		Layout:  VBox{MarginsZero: true},
+		StatusBarItems: []StatusBarItem{
+			StatusBarItem{
+				AssignTo: &sbi,
+				Icon:     icon1,
+				Text:     "click",
+				Width:    80,
+				OnClicked: func() {
+					if sbi.Text() == "click" {
+						sbi.SetText("again")
+						sbi.SetIcon(icon2)
+					} else {
+						sbi.SetText("click")
+						sbi.SetIcon(icon1)
+					}
+				},
+			},
+			StatusBarItem{
+				Text:        "notify",
+				ToolTipText: "no tooltip for me",
+				OnClicked: func() {
+					notify()
+				},
+			},
+			StatusBarItem{
+				Text: "\tcenter",
+			},
+			StatusBarItem{
+				Text: "\t\tright",
+			},
+			StatusBarItem{
+				Icon:        icon1,
+				ToolTipText: "An icon with a tooltip",
+			},
+		},
 	}
 
 	// Run the message loop.
